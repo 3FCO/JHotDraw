485e2083a4f9fd7734b5a5cac89bc7f1ee08e5eb
BJLUN21 <bjlun21@student.sdu.dk>
2024-01-04 10:37:20 +0100

    Fixed clean install errors
    removed jhotdraw-tests module, and moved my tests to jhotdraw-samples-misc.

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/undo/Stages/GivenUserAction.java
@@ -0,0 +1,26 @@
+package org.jhotdraw.samples.svg.undo.Stages;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import org.jhotdraw.draw.*;
+import org.jhotdraw.samples.svg.figures.SVGRectFigure;
+import org.jhotdraw.undo.UndoRedoManager;
+
+public class GivenUserAction extends Stage<GivenUserAction> {
+
+    @ProvidedScenarioState
+    Drawing drawing;
+    @ProvidedScenarioState
+    UndoRedoManager undoRedoManager;
+    @ProvidedScenarioState
+    SVGRectFigure rect;
+
+    public GivenUserAction the_user_has_made_a_mistake() {
+        drawing = new DefaultDrawing();
+        undoRedoManager = new UndoRedoManager();
+        drawing.addUndoableEditListener(undoRedoManager);
+        rect = new SVGRectFigure(10, 10, 50, 100);
+        drawing.add(rect);
+        return self();
+    }
+}
